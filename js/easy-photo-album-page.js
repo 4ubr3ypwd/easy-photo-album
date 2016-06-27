9238c750be4dab5f64e1d8900e41acea10e73faa
Aubrey Portwood <aubreypwd@gmail.com>
2016-06-27 08:30:14 -0700

    Cleanup
    Some basic cleanup of JS, CSS, and .txt files I found. Mainly converting
    tabs, cleaning CSS, removing unneeded comments, etc.

--- js/easy-photo-album-page.js
+++ js/easy-photo-album-page.js
@@ -1,23 +1,3 @@
-/*
-Easy Photo Album Wordpress plugin javascript.
-
-Copyright (C) 2013  TV productions
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- */
-
 // Support for string.format function.
 if (!String.prototype.format) {
 	String.prototype.format = function() {
@@ -414,5 +394,4 @@ window.TVproductions = window.TVproductions || {};
 
 	// } (end public)
 
-})(window.TVproductions.EasyPhotoAlbum = window.TVproductions.EasyPhotoAlbum
-		|| {}, jQuery);
+})(window.TVproductions.EasyPhotoAlbum = window.TVproductions.EasyPhotoAlbum || {}, jQuery);
