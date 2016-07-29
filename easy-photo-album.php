cb8e673369eaf1252a4cf62fa6c613c48384462d
Aubrey Portwood <aubreypwd@gmail.com>
2016-07-29 11:38:59 -0700

    Remove the inline comment style so our deploy script works

--- easy-photo-album.php
+++ easy-photo-album.php
@@ -1,13 +1,13 @@
 <?php
 /*
- * Plugin Name: Easy Photo Album
- * Version: 1.3.6
- * Author: Aubrey Portwood
- * Author URI: http://profiles.wordpress.org/aubreypwd
- * Description: This plugin makes it very easy to create and manage photo albums. The albums are responsive and display in a lightbox. You can help by submit bugs and request new features at the plugin page at wordpress.org.
- * Licence: GPL3
- * Text Domain:   epa
- * Domain Path:   /lang/
+Plugin Name: Easy Photo Album
+Version: 1.3.6
+Author: Aubrey Portwood
+Author URI: http://profiles.wordpress.org/aubreypwd
+Description: This plugin makes it very easy to create and manage photo albums. The albums are responsive and display in a lightbox. You can help by submit bugs and request new features at the plugin page at wordpress.org.
+Licence: GPL3
+Text Domain:   epa
+Domain Path:   /lang/
  */
 
 /*
